eb105e30cb693c7db0a1220fe53e01873b78a8a5
AlexZhebov <sasha_zhebov@mail.ru>
2022-03-04 09:18:58 +0300

    2

--- classic/src/view/main/Config.js
+++ classic/src/view/main/Config.js
@@ -1,9 +1,38 @@
 Ext.define('AppName.view.main.Config', {
-        extend: 'Ext.Button',
+        extend: 'Ext.panel.Panel',
         xtype: 'configform',
 
-        text: 'My Button',
-        handler: function() {
-            alert('click');
-        }
+        items: [
+            {
+                xtype: 'button',
+                text: 'Кнопка перед панелью',
+
+                listeners: {
+                    click: {
+                        fn: function () {
+                            //Handle click event
+                            alert('Нажата кнопка перед панелью');
+                            }
+                        }
+                    }
+            },
+            {
+                xtype: 'panel',
+                title: 'Добавление персоны',
+                height: 200,
+                items: [
+                    {
+                        xtype: 'textfield',
+                        name: 'name',
+                        fieldLabel: 'Name',
+                        allowBlank: false  // requires a non-empty value
+                    },
+                    {
+                        xtype: 'button',
+                        text: 'Кнопка'
+                    }
+                ]
+            }
+        ]
+
     });
